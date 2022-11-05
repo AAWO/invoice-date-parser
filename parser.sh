0c0426b3a482408bf669da5b1886f86e9751803a
Andrzej Wojciechowski <AWojciechowski93@gmail.com>
2022-11-05 15:31:19 +0100

    Added missing script header

--- parser.sh
+++ parser.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Description  : A PDF invoice dates parser
+# Argument     : Path to a PDF invoice file
+# Author       : Andrzej Wojciechowski (AAWO)
+# License      : GNU General Public License
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
